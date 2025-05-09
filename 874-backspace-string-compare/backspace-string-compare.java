835a0dd3673146789e06411b39cc47908ec3a442
MOHANRAJ R <148096405+Mohanraj-0101@users.noreply.github.com>
2025-05-09 16:01:51 +0530

    Time: 2 ms (50.93%) | Memory: 41.8 MB (51.62%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,34 +1,26 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+
+    private String build(String str){
         Stack<Character> stk = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '#') {
-                if (!stk.isEmpty())
+
+        for(char c : str.toCharArray()){
+            if(c == '#'){
+                if(!stk.isEmpty()){
                     stk.pop();
-            } else {
-                stk.push(c);
+                }
+            }else{
+                stk.push(c);    
             }
         }
-        int i = 0;
-        char[] arr1 = new char[stk.size()];
-        while (!stk.isEmpty()) {
-            arr1[i++] = stk.pop();
+
+        StringBuilder sb = new StringBuilder();
+        while(!stk.isEmpty()){
+            sb.append(stk.pop());
         }
 
-        for (char c : t.toCharArray()) {
-            if (c == '#') {
-                if (!stk.isEmpty())
-                    stk.pop();
-            } else {
-                stk.push(c);
-            }
-        }
-        i = 0;
-        char[] arr2 = new char[stk.size()];
-        while (!stk.isEmpty()) {
-            arr2[i++] = stk.pop();
-        }
-
-        return new String(arr1).equals(new String(arr2));
+        return sb.toString();
     }
 }
